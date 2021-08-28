3744535f4d63ab0e1c7ad6f486ff1d794f3576d6
Alpyne <alpyne.dreams@gmail.com>
2021-08-27 18:21:31 -0700

    Fix CategoryChip on light theme

--- components/CategoryChip.js
+++ components/CategoryChip.js
@@ -1,21 +1,22 @@
 import React from 'react'
 import { StyleSheet, Text } from 'react-native'
-import { Chip } from 'react-native-paper'
+import { Chip, useTheme } from 'react-native-paper'
 import { categories as CATEGORIES } from '../data/Categories'
 import { Row } from './Util'
 
 export default function CategoryChip({category, selectable = false, onChange = (() => {})}) {
+  const theme = useTheme()
   const [active, setActive] = React.useState(!selectable)
   const c = category in CATEGORIES ? CATEGORIES[category] : {}
   
-  const style = active ? {backgroundColor: c.color} : {}
+  const style = active && c.color ? {backgroundColor: c.color} : {}
   // Outlined: {borderColor: c.color, color: c.color, borderWidth: 1.5}
 
   return (
     <Chip
       mode={active ? 'flat' : 'outlined'}
       style={[styles.category, style]}
-      textStyle={[styles.categoryText, active ? {color: 'white'} : null]}
+      textStyle={[styles.categoryText, active && c.color ? {color: 'white'} : null]}
       onPress={selectable ? () => {
         setActive(!active)
         onChange(category, !active)
