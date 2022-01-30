560fb3de85231fb30520e694468f3d025a68a013
Alpyne <alpyne.dreams@gmail.com>
2022-01-29 19:28:34 -0800

    Proper color on SubstanceChip icons
    Use substance.color rather than color provided by <Chip>

--- components/substance/SubstanceChip.js
+++ components/substance/SubstanceChip.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useNavigation } from '@react-navigation/native'
 import { Chip, ThemeProvider, useTheme } from 'react-native-paper'
 import Substances from '../../store/Substances'
+import { Icon } from '../Icon'
 
 export default function SubstanceChip({substance: id}) {
 
@@ -9,12 +10,13 @@ export default function SubstanceChip({substance: id}) {
   const navigation = useNavigation()
   
   const substance = Substances[id]
-  const color = substance.color
+  const color = substance?.color
+  const icon = substance?.icon
 
   return (
     <Chip
       mode='outlined'
-      icon={substance.icon}
+      icon={icon ? ({size, color: iconColor}) => <Icon icon={icon} color={color ?? iconColor} size={size} /> : null}
       onPress={() => navigation.navigate('Substance', {substance: id})}
       style={color ? {
         backgroundColor: color.slice(0, 7) + '40',
